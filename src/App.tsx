1fc9ef4e422b9dcba6d4d90c660b6532a27b354b
Patrick Morais <ppternunes@gmail.com>
2021-07-18 14:50:10 -0300

    add styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const Title = styled.h1`
+  color: #8257e6;
+  font-size: 64px;
+`
 function App() {
   return (
-    <div className="App">
+    <Title>
       Hello, world
-    </div>
+    </Title>
   );
 }
 
