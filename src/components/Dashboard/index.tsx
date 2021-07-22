5631145060f7888cd5c5c049b0235a38fd4a2577
Patrick Morais <ppternunes@gmail.com>
2021-07-22 19:37:46 -0300

    create TransactionsTable

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Summary from '../Summary';
+import TransactionsTable from '../TransactionsTable';
 import { Container } from './style';
 
 const Dashboard = () => {
   return (
     <Container>
       <Summary />
+      <TransactionsTable />
     </Container>
   )
 }
