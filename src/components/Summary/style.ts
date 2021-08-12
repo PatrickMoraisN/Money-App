e24e0069963b093d70c843a1ab1122c78c345a34
Patrick Morais <ppternunes@gmail.com>
2021-08-12 17:25:48 -0300

    change style header / summary

--- src/components/Summary/style.ts
+++ src/components/Summary/style.ts
@@ -32,9 +32,7 @@ export const Container = styled.div`
     }
 
     &.red-background {
-      background: #ED213A;  /* fallback for old browsers */
-      background: -webkit-linear-gradient(to bottom, #93291E, #ED213A);  /* Chrome 10-25, Safari 5.1-6 */
-      background: linear-gradient(to bottom, #93291E, #ED213A); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+      background: #ED213A;
       color: #fff;
     }
   }
